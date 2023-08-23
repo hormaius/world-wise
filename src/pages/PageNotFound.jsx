a7e822e8b0638e7641bfe933d797b765b3397fa2
hormaius <88102936+hormaius@users.noreply.github.com>
2023-08-23 21:21:18 +0300

    Initial commit

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav";
+
+function PageNotFound() {
+  return (
+    <div>
+      <PageNav />
+      ERROR: PAGE NOT FOUND!
+    </div>
+  );
+}
+
+export default PageNotFound;
